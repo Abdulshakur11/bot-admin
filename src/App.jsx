8f7ba39a596a98a9cef8b561ed50fe998844e623
Abdushukur <abdulshakurturdimatov@gmail.com>
2022-02-10 21:25:53 +0500

    jdhn

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+
+	const [data, setData] = useState();
+
+	useEffect(() => {
+		fetch('http://localhost:9000/api/v1')
+			.then(res => res.json())
+			.then(data => setData(data));
+	}, [])
+
+
+
+	return (
+		<>
+			<div className="table-wrapper">
+				<table>
+					<caption>City Education</caption>
+					<thead>
+						<tr>
+							<th scope="col">Ismi</th>
+							<th scope="col">Telefon raqami</th>
+							<th scope="col">Kursi</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							data && data.map((e, i) => (
+								<tr key={i}>
+									<td data-label="Kursi">{e.name}</td>
+									<td data-label="Telefon raqami"><a href={`tel:${e.phoneNumber}`}>{e.phoneNumber}</a></td>
+									<td data-label="Ismi">{e.course}</td>
+								</tr>
+							))
+						}
+					</tbody>
+				</table>
+			</div>
+		</>
+	);
+}
+
+export default App;
