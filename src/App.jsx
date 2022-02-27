fece16f2a4a9c1905d849bec65b64f74982b6767
Abdushukur <abdulshakurturdimatov@gmail.com>
2022-02-27 15:50:51 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,21 @@ function App() {
 	const [data, setData] = useState();
 
 	useEffect(() => {
-		fetch('https://internationbot.herokuapp.com/api/v1')
+		fetch('https://internation-bot-73twa.ondigitalocean.app/api/v1')
 			.then(res => res.json())
 			.then(data => setData(data));
 	}, [])
 
+	// const onClilckBtn = (e) => {
+	// 	console.log(e.target.dataset.id);
+	// 	fetch(`http://localhost:9000/api/v2?id=${e.target.dataset.id}`)
+	// }
 
 	return (
 		<>
 			<div className="table-wrapper">
+					<h2 className="title">City Education</h2>
 				<table>
-					<caption>City Education</caption>
 					<thead>
 						<tr>
 							<th scope="col">Ismi</th>
@@ -31,7 +35,9 @@ function App() {
 								<tr key={i}>
 									<td data-label="Kursi">{e.student_name}</td>
 									<td data-label="Telefon raqami"><a href={`tel:${e.phonenumber}`}>{e.phonenumber}</a></td>
-									<td data-label="Ismi">{e.course_name}</td>
+									<td data-label="Ismi">{e.course_name}
+										{/* <button data-id={e.course_id} className="delete-btn">Delete</button> */}
+									</td>
 								</tr>
 							))
 						}
